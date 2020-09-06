ff02b355b4ece3b76fbda525a04ae4e4b9262c6b
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2020-09-06 14:45:52 -0400

    functions tutorial finished

---
+++ functions.py
@@ -0,0 +1,30 @@
+## Q1 --------------------------------------
+def area(width, height):
+    result = width * height
+    return result
+
+## Q2 --------------------------------------
+result = area(5, 6)
+print(result)
+
+## Q3 --------------------------------------
+room_floor_area = area(10, 10)
+tv_surface_area = area(6, 4)
+
+## Q4 --------------------------------------
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+## Q5 --------------------------------------
+debt = subtract(150, 200)
+profit = subtract(500, 100)
+
+## Q7 --------------------------------------
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+money_awarded = add(2000, 1500)
+total_planets = add(4, 4)
+print(money_awarded, total_planets)
